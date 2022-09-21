8f78804221a4280178af42afa51ee604207ce7dd
t-akira012 <aki012.tech@gmail.com>
2022-09-21 16:39:42 +0900

    未使用のkeycodeを削除

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,           KC_W, KC_E,   KC_R,            KC_T,             KC_Y,            KC_U,            KC_I,    KC_O,    KC_P,
       LT(1,KC_A),     KC_S, KC_D,   KC_F,            KC_G,             KC_H,            KC_J,            KC_K,    KC_L,    KC_SCLN,
       LSFT_T(KC_Z),   KC_X, KC_C,   KC_V,            KC_B,             KC_N,            KC_M,            KC_COMM, KC_DOT,  LT(2, KC_SLSH),
-                            KC_TAB, LGUI_T(KC_ESC),  LCTL_T(KC_SPC),   KC_L1_ENT,       LSFT_T(KC_ENT),  KC_SLSH
+                            KC_TAB, LGUI_T(KC_ESC),  LCTL_T(KC_SPC),   KC_L1_ENT,       LSFT_T(KC_ENT),  KC_NO
 ),
 
 /* Raise */
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Lower */
 [_LOWER] = LAYOUT_split_3x5_3(
       KC_TAB,  KC_LSFT, KC_UP,   KC_NO,   KC_BSPC,       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
-      KC_ESC,  KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+      KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
       KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,
                         KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
