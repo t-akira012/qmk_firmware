05a8fa4879ef7b54253d834cb5e44013a6373553
t-akira012 <aki012.tech@gmail.com>
2022-09-21 14:08:53 +0900

    fix miniaxe keymap

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -47,12 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 
-/* todo ; を KC_L1_HOLD と同居させる*/
 [_QWERTY] = LAYOUT_split_3x5_3(
-      KC_Q,         KC_W, KC_E,  KC_R,            KC_T,             KC_Y,            KC_U,            KC_I,    KC_O,    KC_P,
-      LSFT_T(KC_A), KC_S, KC_D,  KC_F,            KC_G,             KC_H,            KC_J,            KC_K,    KC_L,    KC_SCLN,
-      LT(1,KC_Z),   KC_X, KC_C,  KC_V,            KC_B,             KC_N,            KC_M,            KC_COMM, KC_DOT,  KC_L1_HOLD,
-                          KC_NO, LGUI_T(KC_LNG2), LCTL_T(KC_SPC),   LT(2,KC_ENT),    KC_L1_HOLD,      KC_SLSH
+      KC_Q,           KC_W, KC_E,   KC_R,            KC_T,             KC_Y,            KC_U,            KC_I,    KC_O,    KC_P,
+      LT(1,KC_A),     KC_S, KC_D,   KC_F,            KC_G,             KC_H,            KC_J,            KC_K,    KC_L,    KC_SCLN,
+      LSFT_T(KC_Z),   KC_X, KC_C,   KC_V,            KC_B,             KC_N,            KC_M,            KC_COMM, KC_DOT,  LT(2, KC_SLSH),
+                            KC_ESC, LGUI_T(KC_LNG2), LCTL_T(KC_SPC),   LSFT_T(KC_ENT),  KC_L1_HOLD,      KC_SLSH
 ),
 
 /* Raise
@@ -116,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LSFT_T(KC_A):
+        case LSFT_T(KC_Z):
             return true;
         default:
             return false;
