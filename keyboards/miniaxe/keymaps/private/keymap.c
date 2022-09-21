538a62bf1d10a06af7c4159e2bb94b4ff8e0b0c0
t-akira012 <aki012.tech@gmail.com>
2022-09-21 14:37:38 +0900

    RAISE の KC_TRANS を修正

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,           KC_W, KC_E,   KC_R,            KC_T,             KC_Y,            KC_U,            KC_I,    KC_O,    KC_P,
       LT(1,KC_A),     KC_S, KC_D,   KC_F,            KC_G,             KC_H,            KC_J,            KC_K,    KC_L,    KC_SCLN,
       LSFT_T(KC_Z),   KC_X, KC_C,   KC_V,            KC_B,             KC_N,            KC_M,            KC_COMM, KC_DOT,  LT(2, KC_SLSH),
-                            KC_TAB, LGUI_T(KC_ESC),  LCTL_T(KC_SPC),   KC_L1_ENT,      LSFT_T(KC_ENT),  KC_SLSH
+                            KC_TAB, LGUI_T(KC_ESC),  LCTL_T(KC_SPC),   KC_L1_ENT,       LSFT_T(KC_ENT),  KC_SLSH
 ),
 
 /* Raise
@@ -70,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_1,           KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
       KC_GRV,         KC_LBRC, KC_RBRC, KC_LBRC, KC_RBRC,       KC_MINS, KC_PLUS, KC_DQUO, KC_QUOT, KC_COLN,
       KC_LSFT,        KC_LPRN, KC_RPRN, KC_LT,   KC_GT,         KC_UNDS, KC_EQL,  KC_BSLS, KC_NO,   KC_NO,
-                               KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, MO(3),   KC_TRNS
+                               KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Lower
