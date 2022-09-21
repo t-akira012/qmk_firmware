f97d2a52a957d5c0d0662588b28229ee2825046c
t-akira012 <aki012.tech@gmail.com>
2022-09-22 07:26:04 +0900

    Change miniaxe keymap

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -29,7 +29,8 @@ enum custom_keycodes {
   RAISE,
   ADJUST,
   KC_L1_HOLD,
-  NN_LANG2,
+  NN_L2_GUI,
+  NN_ESC,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -37,24 +38,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty */
 [_QWERTY] = LAYOUT_split_3x5_3(
       KC_Q,           KC_W, KC_E,            KC_R,        KC_T,             KC_Y,            KC_U,             KC_I,    KC_O,    KC_P,
-      LSFT_T(KC_A),   KC_S, KC_D,            KC_F,        KC_G,             KC_H,            KC_J,             KC_K,    KC_L,    KC_SCLN,
-      LT(1,KC_Z),     KC_X, KC_C,            KC_V,        KC_B,             KC_N,            KC_M,             KC_COMM, KC_DOT,  LT(2, KC_SLSH),
-                            KC_ESC,          NN_LANG2,    LCTL_T(KC_SPC),   LSFT_T(KC_ENT),  LALT_T(KC_LANG1), KC_COLN
+      LT(1,KC_A),     KC_S, KC_D,            KC_F,        KC_G,             KC_H,            KC_J,             KC_K,    KC_L,    KC_SCLN,
+      LSFT_T(KC_Z),   KC_X, KC_C,            KC_V,        KC_B,             KC_N,            KC_M,             KC_COMM, KC_DOT,  LT(2, KC_SLSH),
+                            NN_ESC,          NN_L2_GUI,   LCTL_T(KC_SPC),   LSFT_T(KC_ENT),  LGUI_T(KC_LANG1), MO(3)
 ),
 
 /* Raise */
 [_RAISE] = LAYOUT_split_3x5_3(
-      KC_1,           KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-      LSFT_T(KC_GRV), KC_LBRC, KC_RBRC, KC_LBRC, KC_RBRC,       KC_MINS, KC_PLUS, KC_DQUO, KC_QUOT, KC_COLN,
-      KC_NO,          KC_LPRN, KC_RPRN, KC_LT,   KC_GT,         KC_UNDS, KC_EQL,  KC_BSLS, KC_NO,   KC_NO,
-                               KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
+      KC_1,       KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+      KC_GRV,     KC_LBRC, KC_RBRC, KC_LBRC, KC_RBRC,       KC_MINS, KC_PLUS, KC_DQUO, KC_QUOT, KC_COLN,
+      KC_LSFT,    KC_LPRN, KC_RPRN, KC_LT,   KC_GT,         KC_UNDS, KC_EQL,  KC_BSLS, KC_NO,   KC_NO,
+                           KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Lower */
 [_LOWER] = LAYOUT_split_3x5_3(
-      KC_TAB,  KC_ESC,  KC_UP,   KC_LGUI,     KC_BSPC,       KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,
-      KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT,     KC_NO,         KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-      KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,         KC_F11,  KC_F12,  KC_TRNS, KC_TRNS, KC_TRNS,
+      KC_TAB,  KC_LSFT, KC_UP,   KC_LGUI,     KC_BSPC,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT,     NN_ESC,        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                         KC_TRNS, KC_TRNS,     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
@@ -85,7 +86,14 @@ bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-    case NN_LANG2:
+    case NN_ESC:
+      if(record->event.pressed) {
+          tap_code(KC_LANG2);
+          tap_code(KC_ESC);
+      }
+      return false;
+      break;
+    case NN_L2_GUI:
       if(record->event.pressed) {
           tap_code(KC_LANG2);
           register_code(KC_LEFT_GUI);
