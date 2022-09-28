27995e3f73bd0cbe653b7128fa8e9569dc87e7fa
t-akira012 <aki012.tech@gmail.com>
2022-09-28 17:50:21 +0900

    Change miniaxe keymap

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -41,24 +41,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty */
 [_QWERTY] = LAYOUT_split_3x5_3(
       KC_Q,           KC_W,  KC_E,            KC_R,            KC_T,             KC_Y,            KC_U,         KC_I,    KC_O,    KC_P,
-      LCTL_T(KC_A),   KC_S,  KC_D,            KC_F,            KC_G,             KC_H,            KC_J,         KC_K,    KC_L,    LCTL_T(KC_SCLN),
-      LSFT_T(KC_Z),   KC_X,  KC_C,            KC_V,            KC_B,             KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(2, KC_SLSH),
+      LSFT_T(KC_A),   KC_S,  KC_D,            KC_F,            KC_G,             KC_H,            KC_J,         KC_K,    KC_L,    KC_SCLN,
+      LCTL_T(KC_Z),   KC_X,  KC_C,            KC_V,            KC_B,             KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(2,KC_SLSH),
                              LALT_T(KC_TAB),  NN_L2_ESC_GUI,   LT(1,KC_SPC),     LSFT_T(KC_ENT),  NN_LANG1_GUI, LT(3,KC_TAB)
 ),
 
 /* Lower */
 [_LOWER] = LAYOUT_split_3x5_3(
       KC_TAB,  KC_LSFT, KC_UP,   KC_LGUI,     KC_LGUI,       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-      KC_LCTL, KC_LEFT, KC_DOWN, KC_RGHT,     KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
-      KC_LSFT, KC_NO,   KC_NO,   KC_NO,       KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      KC_LSFT, KC_LEFT, KC_DOWN, KC_RGHT,     KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+      KC_LCTL, KC_NO,   KC_NO,   KC_NO,       KC_NO,         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                         KC_TRNS, KC_TRNS,     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Raise */
 [_RAISE] = LAYOUT_split_3x5_3(
       KC_1,       KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-      KC_GRV,     KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
-      KC_LSFT,    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,         KC_UNDS, KC_PLUS, KC_BSLS, KC_BSPC, KC_NO,
+      KC_LSFT,    KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
+      KC_LCTL,    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,         KC_UNDS, KC_PLUS, KC_BSLS, KC_GRV,  KC_NO,
                            KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Adjust (Lower + Raise) */
