5bcb02fbf0c1b4da108f66bff0b544c8696285ba
t-akira012 <aki012.tech@gmail.com>
2022-09-28 19:37:27 +0900

    add NN_LANG1_ALT

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -34,6 +34,7 @@ enum custom_keycodes {
   NN_L2_ESC_GUI,
   NN_LANG1,
   NN_LANG1_GUI,
+  NN_LANG1_ALT,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -43,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,           KC_W,  KC_E,            KC_R,            KC_T,             KC_Y,            KC_U,         KC_I,    KC_O,    KC_P,
       LSFT_T(KC_A),   KC_S,  KC_D,            KC_F,            KC_G,             KC_H,            KC_J,         KC_K,    KC_L,    KC_SCLN,
       LCTL_T(KC_Z),   KC_X,  KC_C,            KC_V,            KC_B,             KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(2,KC_SLSH),
-                             LALT_T(KC_TAB),  NN_L2_ESC_GUI,   LT(1,KC_SPC),     LSFT_T(KC_ENT),  NN_LANG1_GUI, LT(3,KC_TAB)
+                             LALT_T(KC_TAB),  NN_L2_ESC_GUI,   LT(1,KC_SPC),     LSFT_T(KC_ENT),  NN_LANG1_ALT, LT(3,KC_TAB)
 ),
 
 /* Lower */
@@ -105,6 +106,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         tap_code(KC_LANG1);
         lang1_on = true;
+      }
+      return false;
+      break;
+    case NN_LANG1_ALT:
+      if (record->event.pressed) {
+        tap_code(KC_LANG1);
+        lang1_on = true;
+        register_code(KC_RIGHT_ALT);
+      } else {
+        unregister_code(KC_RIGHT_ALT);
       }
       return false;
       break;
