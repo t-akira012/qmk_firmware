597168039ca1d8b40757dc9db70826425d1f575d
t-akira012 <aki012.tech@gmail.com>
2022-09-29 14:16:18 +0900

    Q の位置に KC_TAB を配置

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -44,21 +44,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty */
 [_QWERTY] = LAYOUT_split_3x5_3(
-      KC_Q,           KC_W,          KC_E,            KC_R,            KC_T,               KC_Y,            KC_U,         KC_I,    KC_O,    KC_P,
-      LCTL_T(KC_A),   KC_S,          KC_D,            KC_F,            KC_G,               KC_H,            KC_J,         KC_K,    KC_L,    KC_SCLN,
-      LSFT_T(KC_Z),   LALT_T(KC_X),  KC_C,            KC_V,            KC_B,               KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(2,KC_SLSH),
-                                     LALT_T(KC_TAB),  NN_L2_ESC_GUI,   LCTL_T(KC_SPC),     LSFT_T(KC_ENT),  NN_LANG1_LOW, LT(3,KC_TAB)
+      KC_TAB,         KC_W,          KC_E,      KC_R,            KC_T,               KC_Y,            KC_U,         KC_I,    KC_O,    KC_P,
+      LCTL_T(KC_A),   KC_S,          KC_D,      KC_F,            KC_G,               KC_H,            KC_J,         KC_K,    KC_L,    KC_Q,
+      LSFT_T(KC_Z),   LALT_T(KC_X),  KC_C,      KC_V,            KC_B,               KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(2,KC_SCLN),
+                                     KC_LALT,   NN_L2_ESC_GUI,   LCTL_T(KC_SPC),     LSFT_T(KC_ENT),  NN_LANG1_LOW, LT(3,KC_SLSH)
 ),
 
 /* Lower */
 /*  1 2 3 4 5  6 7 8 9 0
     ` ( ) { }  - = " ' :
-    ~ [ ] < >  _ + | \
+    ~ [ ] < >  _ + | \ ;
  */
 [_LOWER] = LAYOUT_split_3x5_3(
       KC_1,       KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
       KC_GRV,     KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
-      KC_TILD,    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,         KC_UNDS, KC_PLUS, KC_PIPE, KC_BSLS, KC_SLSH,
+      KC_TILD,    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,         KC_UNDS, KC_PLUS, KC_PIPE, KC_BSLS, KC_SCLN,
                         KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
