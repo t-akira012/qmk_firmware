64398988a484b97f2dbe1fd1f3b90e58f59b9e30
t-akira012 <aki012.tech@gmail.com>
2022-09-30 22:41:40 +0900

    記号配列の調整

--- keyboards/miniaxe/config.h
+++ keyboards/miniaxe/config.h
@@ -21,9 +21,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* private option */
 #define IGNORE_MOD_TAP_INTERRUPT_PER_KEY
-#define TAPPING_TERM 200
+// #define TAPPING_TERM 50
 #define RETRO_TAPPING
-// #define PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
 
 /* key matrix size */
 #define MATRIX_ROWS 8
--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -47,31 +47,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_split_3x5_3(
       KC_TAB,         KC_W,          KC_E,      KC_R,            KC_T,               KC_Y,            KC_U,         KC_I,    KC_O,    KC_P,
       LCTL_T(KC_A),   KC_S,          KC_D,      KC_F,            KC_G,               KC_H,            KC_J,         KC_K,    KC_L,    KC_Q,
-      LSFT_T(KC_Z),   LALT_T(KC_X),  KC_C,      KC_V,            KC_B,               KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(2,KC_SCLN),
-                                     KC_LALT,   NN_L2_ESC_GUI,   LCTL_T(KC_SPC),     LT(1,KC_ENT),    NN_LANG1_SFT, LT(3,KC_SLSH)
+      LSFT_T(KC_Z),   LALT_T(KC_X),  KC_C,      KC_V,            KC_B,               KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(2,KC_SLSH),
+                                     KC_LALT,   NN_L2_ESC_GUI,   LCTL_T(KC_SPC),     LT(1,KC_ENT),    NN_LANG1_SFT, LT(3,KC_SCLN)
 ),
 
 /* Lower */
 /*  1 2 3 4 5  6 7 8 9 0
-    ` ( ) { }  - = " ' :
-    ~ [ ] < >  _ + | \ ;
+    | ( ) { }  - = " ' :
+    ~ [ ] < >  _ + \ ; BS
  */
 [_LOWER] = LAYOUT_split_3x5_3(
       KC_1,       KC_2,    KC_3,    KC_4,    KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-      KC_GRV,     KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
-      KC_TILD,    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,         KC_UNDS, KC_PLUS, KC_PIPE, KC_BSLS, KC_SCLN,
+      KC_PIPE,    KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
+      KC_TILD,    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,         KC_UNDS, KC_PLUS, KC_SLSH, KC_SCLN, KC_BSPC,
                         KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Raise */
-/*  ! @ # $ %  ^ & * ? BS
-    ` ( ) { }  - = " ' :
-    ~ [ ] < >  _ + | \
+/*  ! @ # $ %  ^ & * ? `
+    | ( ) { }  - = " ' :
+    ~ [ ] < >  _ + \ ;
 */
 [_RAISE] = LAYOUT_split_3x5_3(
-      KC_EXLM,    KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, KC_BSPC,
-      KC_GRV,     KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
-      KC_TILD,    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,         KC_UNDS, KC_PLUS, KC_PIPE, KC_BSLS, KC_NO,
+      KC_EXLM,    KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, KC_GRV,
+      KC_PIPE,    KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
+      KC_TILD,    KC_LBRC, KC_RBRC, KC_LT,   KC_GT,         KC_UNDS, KC_PLUS, KC_BSLS, KC_SCLN, KC_NO,
                            KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Adjust (Lower + Raise) */
@@ -85,16 +85,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool get_ignore_mod_tap_interrupt(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case LSFT_T(KC_Z):
-            return true;
-        case LCTL_T(KC_A):
-            return true;
-        case LCTL_T(KC_SCLN):
-            return true;
-        case LCTL_T(KC_GRV):
-            return true;
-        default:
+        case LT(2,KC_SLSH):
             return false;
+        default:
+            return true;
     }
 }
 
