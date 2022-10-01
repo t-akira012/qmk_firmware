106193ed394f0120f133a17a00a0751a856b63be
t-akira012 <aki012.tech@gmail.com>
2022-10-01 20:06:35 +0900

    lower/raise レイヤーに LSFT を追加

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -57,10 +57,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [ ] < > |  _ + \ ; NO
  */
 [_LOWER] = LAYOUT_split_3x5_3(
-      KC_1,       KC_2,    KC_3,    KC_4,       KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-      KC_LPRN,    KC_RPRN, KC_LCBR, KC_RCBR,    KC_GRV,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
-      KC_LBRC,    KC_RBRC, KC_LT,   KC_GT,      KC_PIPE,       KC_UNDS, KC_PLUS, KC_BSLS, KC_SCLN, KC_NO,
-                           KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
+      KC_1,               KC_2,    KC_3,    KC_4,      KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+      KC_LPRN,            KC_RPRN, KC_LCBR, KC_RCBR,   KC_GRV,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
+      LSFT_T(KC_LBRC),    KC_RBRC, KC_LT,   KC_GT,     KC_PIPE,       KC_UNDS, KC_PLUS, KC_BSLS, KC_SCLN, KC_NO,
+                                   KC_TRNS, KC_TRNS,   KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
 /* Raise */
@@ -69,10 +69,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [ ] < > |  _ + \ ; NO
 */
 [_RAISE] = LAYOUT_split_3x5_3(
-      KC_EXLM,    KC_AT,   KC_HASH, KC_DLR,     KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, KC_BSPC,
-      KC_LPRN,    KC_RPRN, KC_LCBR, KC_RCBR,    KC_GRV,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
-      KC_LBRC,    KC_RBRC, KC_LT,   KC_GT,      KC_PIPE,       KC_UNDS, KC_PLUS, KC_BSLS, KC_SCLN, KC_NO,
-                           KC_TRNS, KC_TRNS,    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
+      KC_EXLM,            KC_AT,   KC_HASH, KC_DLR,    KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, KC_BSPC,
+      KC_LPRN,            KC_RPRN, KC_LCBR, KC_RCBR,   KC_GRV,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
+      LSFT_T(KC_LBRC),    KC_RBRC, KC_LT,   KC_GT,     KC_PIPE,       KC_UNDS, KC_PLUS, KC_BSLS, KC_SCLN, KC_NO,
+                                   KC_TRNS, KC_TRNS,   KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Adjust (Lower + Raise) */
 [_ADJUST] =  LAYOUT_split_3x5_3(
