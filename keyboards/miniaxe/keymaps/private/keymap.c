4ca5f5bea3cb4129411979c36ca454365a553c42
t-akira012 <aki012.tech@gmail.com>
2022-10-02 22:10:55 +0900

    LOWER, RAISE layer のキーマップ調整

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -59,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT_split_3x5_3(
       KC_1,               KC_2,    KC_3,    KC_4,      KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
       KC_LPRN,            KC_RPRN, KC_LCBR, KC_RCBR,   KC_GRV,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_SCLN,
-      LSFT_T(KC_LBRC),    KC_RBRC, KC_LT,   KC_GT,     KC_PIPE,       KC_UNDS, KC_PLUS, KC_TILD, KC_BSLS, KC_NO,
+      KC_LBRC,            KC_RBRC, KC_LT,   KC_GT,     KC_PIPE,       KC_UNDS, KC_PLUS, KC_TILD, KC_BSLS, KC_NO,
                                    KC_TRNS, KC_TRNS,   KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_split_3x5_3(
       KC_EXLM,            KC_AT,   KC_HASH, KC_DLR,    KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, KC_BSPC,
       KC_LPRN,            KC_RPRN, KC_LCBR, KC_RCBR,   KC_GRV,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_SCLN,
-      LSFT_T(KC_LBRC),    KC_RBRC, KC_LT,   KC_GT,     KC_PIPE,       KC_UNDS, KC_PLUS, KC_TILD, KC_BSLS, KC_NO,
+      KC_LBRC,            KC_RBRC, KC_LT,   KC_GT,     KC_PIPE,       KC_UNDS, KC_PLUS, KC_TILD, KC_BSLS, KC_NO,
                                    KC_TRNS, KC_TRNS,   KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* Adjust (Lower + Raise) */
