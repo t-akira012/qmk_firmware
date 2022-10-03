d67ad370a03c4cd4fd6ca777091d7eeca9e644b9
t-akira012 <aki012.tech@gmail.com>
2022-10-03 17:34:59 +0900

    RAISE key を右親指二番目に修正

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,           KC_W,  KC_E,      KC_R,            KC_T,               KC_Y,            KC_U,         KC_I,    KC_O,    KC_P,
       LCTL_T(KC_A),   KC_S,  KC_D,      KC_F,            KC_G,               KC_H,            KC_J,         KC_K,    KC_L,    KC_COLN,
       LSFT_T(KC_Z),   KC_X,  KC_C,      KC_V,            KC_B,               KC_N,            KC_M,         KC_COMM, KC_DOT,  KC_SLSH,
-                             KC_LALT,   NN_L2_ESC_GUI,   LT(1,KC_SPC),       LT(2,KC_ENT),    NN_LANG1_SFT, LT(3,KC_TAB)
+                             KC_LALT,   NN_L2_ESC_GUI,   LT(1,KC_SPC),       LSFT_T(KC_ENT),  NN_LANG1_RAI, LT(3,KC_TAB)
 ),
 
 /* Lower */
@@ -215,7 +215,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_RAISE);
       } else {
         layer_off(_RAISE);
-        if (hold_low) {
+        if (hold_rai) {
           lang1_on = true;
           tap_code(KC_LANG1);
         }
