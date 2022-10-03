b5aea7e73e6f1b4c3c46c8de93984c43bb85b10a
t-akira012 <aki012.tech@gmail.com>
2022-10-03 17:57:55 +0900

    delte ctl + q to ctl + a

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_split_3x5_3(
       KC_Q,           KC_W,  KC_E,      KC_R,            KC_T,               KC_Y,            KC_U,         KC_I,    KC_O,    KC_P,
       LCTL_T(KC_A),   KC_S,  KC_D,      KC_F,            KC_G,               KC_H,            KC_J,         KC_K,    KC_L,    KC_COLN,
-      LSFT_T(KC_Z),   KC_X,  KC_C,      KC_V,            KC_B,               KC_N,            KC_M,         KC_COMM, KC_DOT,  KC_SLSH,
-                             KC_LALT,   NN_L2_ESC_GUI,   RSFT_T(KC_SPC),     LT(1,KC_ENT),    NN_LANG1_RAI, LT(3,KC_TAB)
+      LSFT_T(KC_Z),   KC_X,  KC_C,      KC_V,            KC_B,               KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(2,KC_SLSH),
+                             KC_LALT,   NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     LT(1,KC_ENT),    NN_LANG1_SFT, LT(3,KC_TAB)
 ),
 
 /* Lower */
@@ -133,19 +133,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   //   }
   // }
 
-  // Q or TAB
   // CTL + Q to CTL + A
   if (keycode == KC_Q) {
     if(hold_sum){
       if (record->event.pressed) {
-        unregister_code(KC_RSFT);
+        unregister_code(KC_RCTL);
         tap_code(KC_TAB);
         return false;
       }
-    }
-    if( hold_ctl && !hold_sum ){
-      tap_code(KC_A);
-      return false;
     }
     if(hold_sus){
       if (record->event.pressed) {
@@ -165,7 +160,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
        }
       return true;
       break;
-    case RSFT_T(KC_SPC):
+    case RCTL_T(KC_SPC):
       hold_esc = false;
       hold_alt = false;
       if (hold_rai) {
