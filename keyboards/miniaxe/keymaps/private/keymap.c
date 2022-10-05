4cef801485d33891d5d1d62bb2d9f558c3abbaf1
t-akira012 <aki012.tech@gmail.com>
2022-10-05 22:20:44 +0900

    LANG1 と ALT を統合

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,           KC_W,  KC_E,      KC_R,            KC_T,               KC_Y,            KC_U,         KC_I,    KC_O,    KC_P,
       LCTL_T(KC_A),   KC_S,  KC_D,      KC_F,            KC_G,               KC_H,            KC_J,         KC_K,    KC_L,    KC_COLN,
       NN_Z,           KC_X,  KC_C,      KC_V,            KC_B,               KC_N,            KC_M,         KC_COMM, KC_DOT,  LT(1,KC_SLSH),
-                             KC_LALT,   NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     LSFT_T(KC_ENT),  NN_LANG1_GUI, LT(3,KC_TAB)
+                             KC_LALT,   NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     LSFT_T(KC_ENT),  NN_LANG1_ALT, LT(3,KC_TAB)
 ),
 
 /* Lower */
