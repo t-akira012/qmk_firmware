29befc5bdccdc5f91e3d886e5056c52886fd1366
t-akira012 <aki012.tech@gmail.com>
2022-10-05 23:57:00 +0900

    右親指キー２番目をRAISEに変更

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -36,6 +36,7 @@ enum custom_keycodes {
   NN_LANG1_GUI,
   NN_LANG1_ALT,
   NN_LANG1_LOW,
+  NN_LANG1_RAI,
   NN_LANG1_SFT,
   NN_ENT,
   NN_RPIN1,
@@ -48,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,           KC_W,  KC_E,      KC_R,            KC_T,               KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,
       LCTL_T(KC_A),   KC_S,  KC_D,      KC_F,            KC_G,               KC_H,    KC_J,         KC_K,    KC_L,    KC_COLN,
       KC_Z,           KC_X,  KC_C,      KC_V,            KC_B,               KC_N,    KC_M,         KC_COMM, KC_DOT,  NN_RPIN1,
-                             KC_BSPC,   NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     NN_ENT,  NN_LANG1_ALT, LT(3,KC_TAB)
+                             KC_BSPC,   NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     NN_ENT,  NN_LANG1_RAI, LT(3,KC_TAB)
 ),
 
 /* Lower */
@@ -230,6 +231,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           tap_code(KC_LANG1);
         }
         hold_sft = false;
+      }
+      return false;
+      break;
+    case NN_LANG1_RAI:
+      if (record->event.pressed) {
+        hold_low = true;
+        layer_on(_RAISE);
+      } else {
+        layer_off(_RAISE);
+        if (hold_low) {
+          lang1_on = true;
+          tap_code(KC_LANG1);
+        }
+        hold_low = false;
       }
       return false;
       break;
