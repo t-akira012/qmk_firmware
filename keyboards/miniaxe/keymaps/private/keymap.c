9b7f6c11d1022e62f99e1b3ff09d0cb98abd1d7d
t-akira012 <aki012.tech@gmail.com>
2022-10-23 12:24:13 +0900

    fix

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -47,9 +47,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Qwerty */
 [_QWERTY] = LAYOUT_split_3x5_3(
       KC_Q,           KC_W,  KC_E,      KC_R,            KC_T,               KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,
-      LCTL_T(KC_A),   KC_S,  KC_D,      KC_F,            KC_G,               KC_H,    KC_J,         KC_K,    KC_L,    KC_SCLN,
+      LCTL_T(KC_A),   KC_S,  KC_D,      KC_F,            KC_G,               KC_H,    KC_J,         KC_K,    KC_L,    LT(3,KC_SCLN),
       LSFT_T(KC_Z),   KC_X,  KC_C,      KC_V,            KC_B,               KC_N,    KC_M,         KC_COMM, KC_DOT,  NN_RPIN1,
-                             KC_LALT,   NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     NN_ENT,  NN_LANG1_SFT, LT(3,KC_BSPC)
+                             KC_LALT,   NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     NN_ENT,  NN_LANG1_SFT, KC_BSPC
 ),
 
 /* Lower */
@@ -77,10 +77,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 /* Adjust (Lower + Raise) */
 [_ADJUST] =  LAYOUT_split_3x5_3(
-  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_F11,  KC_F12,  _______, _______, _______,      _______, _______, _______, KC_UP,   _______,
-  KC_NO,   _______, _______, _______, _______,      KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT,
-                    _______, _______, _______,      _______,  _______, _______
+  KC_F1,           KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_NO,   KC_UP,   KC_NO,   KC_NO,   KC_NO,
+  LCTL_T(KC_F6),   KC_F7,   KC_F8,   KC_F9,   KC_F10,       KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,
+  LSFT_T(KC_F11),  KC_F12,  _______, _______, _______,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                            _______, KC_TRNS, KC_TRNS,      KC_ENT,  KC_TRNS, _______
 )
 };
 
