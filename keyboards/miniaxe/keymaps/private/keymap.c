bd3423dca53971270c9f95df306dca6f4174f7d0
t-akira012 <aki012.tech@gmail.com>
2022-11-07 23:31:04 +0900

    emoji

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -40,6 +40,8 @@ enum custom_keycodes {
   NN_LANG1_SFT,
   NN_ENT,
   NN_RPIN1,
+  EMOJI,
+  CUT,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -49,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,           KC_W,  KC_E,      KC_R,            KC_T,               KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,
       LCTL_T(KC_A),   KC_S,  KC_D,      KC_F,            KC_G,               KC_H,    KC_J,         KC_K,    KC_L,    LT(3,KC_SCLN),
       LSFT_T(KC_Z),   KC_X,  KC_C,      KC_V,            KC_B,               KC_N,    KC_M,         KC_COMM, KC_DOT,  NN_RPIN1,
-                             KC_LALT,   NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     NN_ENT,  NN_LANG1_ALT, KC_BSPC
+                             EMOJI,     NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     NN_ENT,  NN_LANG1_ALT, KC_DEL
 ),
 
 /* Lower */
@@ -61,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_1,         KC_2,    KC_3,    KC_4,      KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
       KC_LPRN,      KC_RPRN, KC_LCBR, KC_RCBR,   KC_GRV,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
       KC_LBRC,      KC_RBRC, KC_LT,   KC_GT,     KC_PIPE,       KC_UNDS, KC_PLUS, KC_TILD, KC_BSLS, KC_NO,
-                             KC_TRNS, KC_TRNS,   KC_TRNS,       MO(_RAISE), KC_TRNS, KC_TRNS
+                             KC_TRNS, KC_TRNS,   KC_TRNS,       MO(_RAISE), KC_TRNS, CUT
 ),
 
 /* Raise */
@@ -73,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,    KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, KC_BSPC,
       KC_LPRN,      KC_RPRN, KC_LCBR, KC_RCBR,   KC_GRV,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
       KC_LBRC,      KC_RBRC, KC_LT,   KC_GT,     KC_PIPE,       KC_UNDS, KC_PLUS, KC_TILD, KC_BSLS, KC_NO,
-                             KC_TRNS, KC_TRNS,   KC_TRNS,       KC_NO,   KC_TRNS, KC_TRNS
+                             KC_TRNS, KC_TRNS,   KC_TRNS,       KC_NO,   KC_TRNS, CUT
 ),
 /* Adjust (Lower + Raise) */
 [_ADJUST] =  LAYOUT_split_3x5_3(
@@ -151,6 +153,30 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   }
 
   switch (keycode) {
+    case CUT:
+      if(record->event.pressed){
+        register_code(KC_LGUI);
+        tap_code(KC_LEFT);
+        unregister_code(KC_LGUI);
+        register_code(KC_LSFT);
+        tap_code(KC_DOWN);
+        tap_code(KC_LEFT);
+      }else{
+        unregister_code(KC_LSFT);
+      }
+      return false;
+      break;
+    case EMOJI:
+      if(record->event.pressed){
+        register_code(KC_LCTL);
+        register_code(KC_LGUI);
+        tap_code(KC_SPC);
+      }else{
+        unregister_code(KC_LCTL);
+        unregister_code(KC_LGUI);
+      }
+      return false;
+      break;
     case KC_A:
       if(record->event.pressed){
           if(hold_sus||hold_sum){
