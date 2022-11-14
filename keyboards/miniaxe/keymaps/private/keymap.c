f199f517f74f2fd0bff20e4a52c3884fbf5ec200
t-akira012 <aki012.tech@gmail.com>
2022-11-14 17:00:38 +0900

    Z に LSFT を同居させる運用を復活

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -48,9 +48,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Qwerty */
 [_QWERTY] = LAYOUT_split_3x5_3(
-      KC_Q,           KC_W,  KC_E,      KC_R,            KC_T,               KC_Y,    KC_U,         KC_I,    KC_O,    LSFT_T(KC_P),
+      KC_Q,           KC_W,  KC_E,      KC_R,            KC_T,               KC_Y,    KC_U,         KC_I,    KC_O,    KC_P,
       KC_A,           KC_S,  KC_D,      KC_F,            KC_G,               KC_H,    KC_J,         KC_K,    KC_L,    LT(3,KC_SCLN),
-      KC_Z,           KC_X,  KC_C,      KC_V,            KC_B,               KC_N,    KC_M,         KC_COMM, KC_DOT,  NN_RPIN1,
+      LSFT_T(KC_Z),   KC_X,  KC_C,      KC_V,            KC_B,               KC_N,    KC_M,         KC_COMM, KC_DOT,  NN_RPIN1,
                              EMOJI,     NN_L2_ESC_GUI,   RCTL_T(KC_SPC),     NN_ENT,  NN_LANG1_ALT, KC_DEL
 ),
 
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_split_3x5_3(
       KC_1,         KC_2,    KC_3,    KC_4,      KC_5,          KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-      KC_LPRN,      KC_RPRN, KC_LCBR, KC_RCBR,   KC_TILD,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
+      KC_LPRN,      KC_RPRN, KC_LCBR, KC_RCBR,   KC_TILD,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
       KC_LBRC,      KC_RBRC, KC_BSLS, KC_GRV,    KC_PIPE,       KC_UNDS, KC_PLUS, KC_COMM, KC_DOT,  KC_NO,
                              KC_TRNS, KC_TRNS,   KC_TRNS,       MO(_RAISE), KC_TRNS, CUT
 ),
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 */
 [_RAISE] = LAYOUT_split_3x5_3(
       KC_EXLM,      KC_AT,   KC_HASH, KC_DLR,    KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_QUES, KC_BSPC,
-      KC_LPRN,      KC_RPRN, KC_LCBR, KC_RCBR,   KC_TILD,        KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
+      KC_LPRN,      KC_RPRN, KC_LCBR, KC_RCBR,   KC_TILD,       KC_MINS, KC_EQL,  KC_DQUO, KC_QUOT, KC_COLN,
       KC_LBRC,      KC_RBRC, KC_BSLS, KC_GRV,    KC_PIPE,       KC_UNDS, KC_PLUS, KC_COMM, KC_DOT,  KC_NO,
                              KC_TRNS, KC_TRNS,   KC_TRNS,       KC_NO,   KC_TRNS, CUT
 ),
@@ -210,7 +210,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         register_code(KC_RSFT);
        } else {
         unregister_code(KC_RSFT);
-        if (hold_rsum1 && timer_elapsed(tap_timer) < 200) {
+        if (hold_rsum1 && timer_elapsed(tap_timer) < 140) {
           tap_code(KC_ENT);
         }
         hold_rsum1 = false;
