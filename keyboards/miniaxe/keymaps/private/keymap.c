3fe32fae945cf1752e84960044cb41a641ead242
t-akira012 <aki012.tech@gmail.com>
2022-12-26 15:08:07 +0900

    fix syntax error

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -20,7 +20,8 @@ enum layer_names {
     _QWERTY,
     _LOWER,
     _RAISE,
-    _ADJUST
+    _ADJUST,
+    _FKEY,
 };
 
 enum custom_keycodes {
@@ -77,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LCTL,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT,    KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,
       KC_LSFT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
                           EMOJI,   KC_TRNS, KC_LCTL,    KC_LSFT, KC_TRNS, KC_TRNS
-)
+),
 /* F Key */
 [_FKEY] =  LAYOUT_split_3x5_3(
       KC_F1,     KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_F6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,
