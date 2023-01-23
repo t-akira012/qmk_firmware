7cd6cb1db9dc00f1c1e48100afd3a7fbb9ab64f3
t-akira012 <aki012.tech@gmail.com>
2023-01-23 12:53:14 +0900

    kc_z を lsft_t 付与

--- keyboards/miniaxe/keymaps/private/keymap.c
+++ keyboards/miniaxe/keymaps/private/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_split_3x5_3(
       KC_Q,    KC_W,        KC_E,      KC_R,       KC_T,             KC_Y,     KC_U,      KC_I,    KC_O,    KC_P,
       KC_A,    LT(3,KC_S),  KC_D,      KC_F,       KC_G,             KC_H,     KC_J,      KC_K,    KC_L,    LT(3,KC_SCLN),
-      KC_Z,    KC_X,        KC_C,      KC_V,       KC_B,             KC_N,     KC_M,      KC_COMM, KC_DOT,  NN_RPIN1,
+      LSFT_T(KC_Z),    KC_X,        KC_C,      KC_V,       KC_B,             KC_N,     KC_M,      KC_COMM, KC_DOT,  NN_RPIN1,
                             KC_LALT,   NN_LSUM2,   RCTL_T(KC_SPC),   NN_RSUM1, NN_RSUM2,  LT(4,KC_DEL)
 ),
 
